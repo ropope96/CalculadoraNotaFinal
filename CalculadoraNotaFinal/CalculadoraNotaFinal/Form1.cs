1941a9678d80d4b20b835a357151b3f7eabefc6d
Clase <Clase@1A-PC17>
2018-01-04 18:37:43 -0500

    Calculadora hecha

--- CalculadoraNotaFinal/CalculadoraNotaFinal/Form1.cs
+++ CalculadoraNotaFinal/CalculadoraNotaFinal/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CalculadoraNotaFinal
@@ -15,6 +9,23 @@ namespace CalculadoraNotaFinal
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var c1 = Convert.ToInt32(textBox1.Text);
+			var c2 = Convert.ToInt32(textBox2.Text);
+			var c3 = Convert.ToInt32(textBox3.Text);
+			var c4 = Convert.ToInt32(textBox4.Text);
+
+			//El .Min no soporta más de dos valores, por lo que ponemos todos los valores en un 
+			// arreglo y luego invocamos a .Min
+			var menorControl = new int[] { c1, c2, c3, c4 }.Min();
+
+			var promedioControles = (c1 + c2 + c3 + c4 - menorControl) / 3;
+
+			MessageBox.Show("El promedio de controles es :" + promedioControles);
 		}
 	}
 }
