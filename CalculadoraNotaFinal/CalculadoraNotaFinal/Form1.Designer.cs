935e1ef4be4b8d217c01bffde65a39af17a53d3e
Clase <Clase@1A-PC17>
2018-01-04 19:27:36 -0500

    Calculadora Nota Final terminada

--- CalculadoraNotaFinal/CalculadoraNotaFinal/Form1.Designer.cs
+++ CalculadoraNotaFinal/CalculadoraNotaFinal/Form1.Designer.cs
@@ -37,12 +37,38 @@
 			this.textBox4 = new System.Windows.Forms.TextBox();
 			this.label4 = new System.Windows.Forms.Label();
 			this.button1 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.textBox10 = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.textBox11 = new System.Windows.Forms.TextBox();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox4.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(35, 40);
+			this.label1.Location = new System.Drawing.Point(20, 37);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(52, 13);
 			this.label1.TabIndex = 0;
@@ -50,14 +76,14 @@
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(104, 37);
+			this.textBox1.Location = new System.Drawing.Point(92, 34);
 			this.textBox1.Name = "textBox1";
 			this.textBox1.Size = new System.Drawing.Size(100, 20);
 			this.textBox1.TabIndex = 1;
 			// 
 			// textBox2
 			// 
-			this.textBox2.Location = new System.Drawing.Point(104, 132);
+			this.textBox2.Location = new System.Drawing.Point(92, 120);
 			this.textBox2.Name = "textBox2";
 			this.textBox2.Size = new System.Drawing.Size(100, 20);
 			this.textBox2.TabIndex = 3;
@@ -65,7 +91,7 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(35, 135);
+			this.label2.Location = new System.Drawing.Point(23, 123);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(52, 13);
 			this.label2.TabIndex = 2;
@@ -73,7 +99,7 @@
 			// 
 			// textBox3
 			// 
-			this.textBox3.Location = new System.Drawing.Point(104, 86);
+			this.textBox3.Location = new System.Drawing.Point(92, 74);
 			this.textBox3.Name = "textBox3";
 			this.textBox3.Size = new System.Drawing.Size(100, 20);
 			this.textBox3.TabIndex = 5;
@@ -81,7 +107,7 @@
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(35, 89);
+			this.label3.Location = new System.Drawing.Point(23, 77);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(52, 13);
 			this.label3.TabIndex = 4;
@@ -89,7 +115,7 @@
 			// 
 			// textBox4
 			// 
-			this.textBox4.Location = new System.Drawing.Point(104, 178);
+			this.textBox4.Location = new System.Drawing.Point(92, 166);
 			this.textBox4.Name = "textBox4";
 			this.textBox4.Size = new System.Drawing.Size(100, 20);
 			this.textBox4.TabIndex = 7;
@@ -97,7 +123,7 @@
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(35, 181);
+			this.label4.Location = new System.Drawing.Point(23, 169);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(52, 13);
 			this.label4.TabIndex = 6;
@@ -105,30 +131,244 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(75, 230);
+			this.button1.Location = new System.Drawing.Point(664, 273);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(129, 23);
 			this.button1.TabIndex = 8;
-			this.button1.Text = "Calcular Nota";
+			this.button1.Text = "Calcular Nota Final";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.textBox1);
+			this.groupBox1.Controls.Add(this.label12);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.textBox4);
+			this.groupBox1.Controls.Add(this.textBox2);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.textBox3);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Location = new System.Drawing.Point(26, 23);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(273, 289);
+			this.groupBox1.TabIndex = 9;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Controles";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label14);
+			this.groupBox2.Controls.Add(this.textBox5);
+			this.groupBox2.Controls.Add(this.label5);
+			this.groupBox2.Controls.Add(this.textBox6);
+			this.groupBox2.Controls.Add(this.textBox7);
+			this.groupBox2.Controls.Add(this.label6);
+			this.groupBox2.Controls.Add(this.label7);
+			this.groupBox2.Controls.Add(this.textBox8);
+			this.groupBox2.Controls.Add(this.label8);
+			this.groupBox2.Location = new System.Drawing.Point(321, 27);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(273, 285);
+			this.groupBox2.TabIndex = 10;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Laboratorios";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(105, 34);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 1;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(20, 37);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(72, 13);
+			this.label5.TabIndex = 0;
+			this.label5.Text = "Laboratorio 1:";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(105, 166);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.TabIndex = 7;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(105, 120);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 3;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(23, 169);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(72, 13);
+			this.label6.TabIndex = 6;
+			this.label6.Text = "Laboratorio 4:";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(23, 123);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(72, 13);
+			this.label7.TabIndex = 2;
+			this.label7.Text = "Laboratorio 3:";
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(105, 74);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(100, 20);
+			this.textBox8.TabIndex = 5;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(23, 77);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(72, 13);
+			this.label8.TabIndex = 4;
+			this.label8.Text = "Laboratorio 2:";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.textBox9);
+			this.groupBox3.Controls.Add(this.label9);
+			this.groupBox3.Location = new System.Drawing.Point(621, 27);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(210, 75);
+			this.groupBox3.TabIndex = 10;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Proyecto";
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(92, 34);
+			this.textBox9.Name = "textBox9";
+			this.textBox9.Size = new System.Drawing.Size(100, 20);
+			this.textBox9.TabIndex = 1;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(20, 37);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(52, 13);
+			this.label9.TabIndex = 0;
+			this.label9.Text = "Proyecto:";
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Controls.Add(this.textBox11);
+			this.groupBox4.Controls.Add(this.label11);
+			this.groupBox4.Controls.Add(this.textBox10);
+			this.groupBox4.Controls.Add(this.label10);
+			this.groupBox4.Location = new System.Drawing.Point(621, 126);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(210, 132);
+			this.groupBox4.TabIndex = 11;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "Exámenes";
+			// 
+			// textBox10
+			// 
+			this.textBox10.Location = new System.Drawing.Point(92, 34);
+			this.textBox10.Name = "textBox10";
+			this.textBox10.Size = new System.Drawing.Size(100, 20);
+			this.textBox10.TabIndex = 1;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(20, 37);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(42, 13);
+			this.label10.TabIndex = 0;
+			this.label10.Text = "Parcial:";
+			// 
+			// textBox11
+			// 
+			this.textBox11.Location = new System.Drawing.Point(92, 70);
+			this.textBox11.Name = "textBox11";
+			this.textBox11.Size = new System.Drawing.Size(100, 20);
+			this.textBox11.TabIndex = 3;
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(20, 73);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(32, 13);
+			this.label11.TabIndex = 2;
+			this.label11.Text = "Final:";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(23, 222);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(115, 13);
+			this.label12.TabIndex = 12;
+			this.label12.Text = "Promedio de controles:";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Location = new System.Drawing.Point(20, 218);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(126, 13);
+			this.label14.TabIndex = 13;
+			this.label14.Text = "Promedio de laboratorios:";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(632, 317);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(135, 13);
+			this.label13.TabIndex = 14;
+			this.label13.Text = "Promedio de evaluaciones:";
+			// 
+			// label15
+			// 
+			this.label15.AutoSize = true;
+			this.label15.Location = new System.Drawing.Point(675, 349);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(55, 13);
+			this.label15.TabIndex = 15;
+			this.label15.Text = "Nota final:";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(264, 300);
+			this.ClientSize = new System.Drawing.Size(847, 386);
+			this.Controls.Add(this.label15);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.groupBox4);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.button1);
-			this.Controls.Add(this.textBox4);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.textBox3);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.label1);
 			this.Name = "Form1";
 			this.Text = "Calculadora de Nota Final";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox4.ResumeLayout(false);
+			this.groupBox4.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -145,6 +385,28 @@
 		public System.Windows.Forms.TextBox textBox4;
 		public System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label5;
+		public System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		public System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.TextBox textBox11;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox textBox10;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label15;
 	}
 }
 
--- CalculadoraNotaFinal/CalculadoraNotaFinal/Form1.cs
+++ CalculadoraNotaFinal/CalculadoraNotaFinal/Form1.cs
@@ -19,13 +19,38 @@ namespace CalculadoraNotaFinal
 			var c3 = Convert.ToInt32(textBox3.Text);
 			var c4 = Convert.ToInt32(textBox4.Text);
 
-			//El .Min no soporta más de dos valores, por lo que ponemos todos los valores en un 
-			// arreglo y luego invocamos a .Min
 			var menorControl = new int[] { c1, c2, c3, c4 }.Min();
 
 			var promedioControles = (c1 + c2 + c3 + c4 - menorControl) / 3;
 
-			MessageBox.Show("El promedio de controles es :" + promedioControles);
+			label12.Text = "El promedio de Controles es :" + promedioControles;
+
+
+			var l1 = Convert.ToInt32(textBox5.Text);
+			var l2 = Convert.ToInt32(textBox8.Text);
+			var l3 = Convert.ToInt32(textBox7.Text);
+			var l4 = Convert.ToInt32(textBox6.Text);
+
+			var promedioLaboratorios = (l1 + l2 + l3 + l4) / 4;
+
+			label14.Text = "El promedio de Laboratorios es :" + promedioLaboratorios;
+
+			var trabajo = Convert.ToInt32(textBox9.Text);
+
+			var promedioEvaluaciones = (promedioLaboratorios + promedioControles + trabajo) / 3;
+
+			label13.Text = "El promedio de Evaluaciones es :" + promedioEvaluaciones;
+
+			var examParcial = Convert.ToInt32(textBox10.Text);
+
+			var examFinal = Convert.ToInt32(textBox11.Text);
+
+			var notaFinal = (2 * promedioEvaluaciones + examParcial + examFinal) / 4;
+
+			label15.Text = "La Nota Final es :" + notaFinal;
+
 		}
+
+
 	}
 }
